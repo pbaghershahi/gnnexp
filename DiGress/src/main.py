c7640a8260084316cb13a94f6055702e6f6fc4d9
Peyman Baghershahi <peyman.baghershahi@gmail.com>
2024-04-16 15:40:34 -0500

    modified

--- DiGress/src/main.py
+++ DiGress/src/main.py
@@ -1,3 +1,5 @@
+#new main.py function to do crossentropy(G^t, G^{t-1})
+
 import os
 import sys
 sys.path.append("..")
@@ -6,9 +8,12 @@ sys.path.append("..")
 import os
 import pathlib
 import warnings
+import ipdb
+import wandb
 
 import torch
 torch.cuda.empty_cache()
+import torch.nn.functional as F
 import hydra
 from omegaconf import DictConfig
 from pytorch_lightning import Trainer
@@ -16,11 +21,170 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.utilities.warnings import PossibleUserWarning
 
 from src import utils
+from src.diffusion import diffusion_utils
 from metrics.abstract_metrics import TrainAbstractMetricsDiscrete, TrainAbstractMetrics
 
 from diffusion_model import LiftedDenoisingDiffusion
 from diffusion_model_discrete import DiscreteDenoisingDiffusion
 from diffusion.extra_features import DummyExtraFeatures, ExtraFeatures
+
+
+class StepwiseDiffusion(DiscreteDenoisingDiffusion):
+    def __init__(self, cfg, dataset_infos, train_metrics, sampling_metrics, visualization_tools, extra_features,
+                 domain_features):
+        super().__init__(cfg, dataset_infos, train_metrics, sampling_metrics, visualization_tools, extra_features,
+                 domain_features)
+
+    def training_step(self, data, i):
+        if data.edge_index.numel() == 0:
+            self.print("Found a batch with no edges. Skipping.")
+            return
+        dense_data, node_mask = utils.to_dense(data.x, data.edge_index, data.edge_attr, data.batch)
+        dense_data = dense_data.mask(node_mask)
+        X, E = dense_data.X, dense_data.E
+        # lowest_t = 1 if self.training else 1
+        lowest_t = 1
+        t_int = torch.randint(lowest_t, self.T + 1, size=(X.size(0), 1), device=X.device).float()  # (bs, 1)
+        noisy_data_pre = self.apply_noise(X, E, data.y, node_mask, t_int-1)
+        noisy_data_next = self.apply_noise(X, E, data.y, node_mask, t_int)
+        extra_data = self.compute_extra_data(noisy_data_next)
+        pred = self.forward(noisy_data_next, extra_data, node_mask)
+        loss = self.train_loss(
+            masked_pred_X=pred.X,
+            masked_pred_E=pred.E,
+            pred_y=pred.y,
+            # true_X=X,
+            true_X=noisy_data_pre["X_t"],
+            # true_E=E,
+            true_E=noisy_data_pre["E_t"],
+            # true_y=data.y,
+            true_y=noisy_data_pre["y_t"],
+            log=i % self.log_every_steps == 0
+            )
+        self.train_metrics(masked_pred_X=pred.X, masked_pred_E=pred.E, true_X=X, true_E=E,
+                           log=i % self.log_every_steps == 0)
+
+        return {'loss': loss}
+
+    # def validation_step(self, data, i):
+    #     dense_data, node_mask = utils.to_dense(data.x, data.edge_index, data.edge_attr, data.batch)
+    #     dense_data = dense_data.mask(node_mask)
+    #     X, E = dense_data.X, dense_data.E
+    #     # lowest_t = 1 if self.training else 1
+    #     lowest_t = 1
+    #     t_int = torch.randint(lowest_t, self.T + 1, size=(X.size(0), 1), device=X.device).float()  # (bs, 1)
+    #     noisy_data = self.apply_noise(dense_data.X, dense_data.E, data.y, node_mask, t_int)
+    #     extra_data = self.compute_extra_data(noisy_data)
+    #     pred = self.forward(noisy_data, extra_data, node_mask)
+    #     nll = self.compute_val_loss(pred, noisy_data, dense_data.X, dense_data.E, data.y,  node_mask, test=False)
+    #     return {'loss': nll}
+
+    def validation_step(self, data, i):
+        dense_data, node_mask = utils.to_dense(data.x, data.edge_index, data.edge_attr, data.batch)
+        dense_data = dense_data.mask(node_mask)
+        t_int = torch.tensor([self.T], device=dense_data.X.device, dtype=float)[:, None].tile(dense_data.X.size(0), 1)
+        noisy_data = self.apply_noise(dense_data.X, dense_data.E, data.y, node_mask, t_int)
+        counter = self.T
+        while counter > 0:
+            extra_data = self.compute_extra_data(noisy_data)
+            pred = self.forward(noisy_data, extra_data, node_mask)
+            counter -= 1
+            t_int -= 1
+            t_float, s_float, beta_t, alpha_s_bar, alpha_t_bar = self.get_t_attr(t_int)
+            noisy_data = {'t': t_float, 't_int': t_int, 'X_t': pred.X, 'E_t': pred.E, 'y_t': pred.y, 'node_mask': node_mask,
+                          'beta_t': beta_t, 'alpha_s_bar': alpha_s_bar, 'alpha_t_bar': alpha_t_bar,}
+        nll = self.compute_val_loss(pred, noisy_data, dense_data.X, dense_data.E, data.y, node_mask, test=True)
+        return {'loss': nll}
+
+    # def test_step(self, data, i):
+    #     dense_data, node_mask = utils.to_dense(data.x, data.edge_index, data.edge_attr, data.batch)
+    #     dense_data = dense_data.mask(node_mask)
+    #     noisy_data = self.apply_noise(dense_data.X, dense_data.E, data.y, node_mask)
+    #     extra_data = self.compute_extra_data(noisy_data)
+    #     pred = self.forward(noisy_data, extra_data, node_mask)
+    #     nll = self.compute_val_loss(pred, noisy_data, dense_data.X, dense_data.E, data.y, node_mask, test=True)
+    #     return {'loss': nll}
+
+    def get_t_attr(self, t_int):
+        s_int = t_int - 1
+        t_float = t_int / self.T
+        s_float = s_int / self.T
+        # beta_t and alpha_s_bar are used for denoising/loss computation
+        beta_t = self.noise_schedule(t_normalized=t_float)                         # (bs, 1)
+        alpha_s_bar = self.noise_schedule.get_alpha_bar(t_normalized=s_float)      # (bs, 1)
+        alpha_t_bar = self.noise_schedule.get_alpha_bar(t_normalized=t_float)      # (bs, 1)
+        return t_float, s_float, beta_t, alpha_s_bar, alpha_t_bar
+
+    def apply_noise(self, X, E, y, node_mask, t_int):
+        t_float, s_float, beta_t, alpha_s_bar, alpha_t_bar = self.get_t_attr(t_int)
+        Qtb = self.transition_model.get_Qt_bar(alpha_t_bar, device=self.device)  # (bs, dx_in, dx_out), (bs, de_in, de_out)
+        assert (abs(Qtb.X.sum(dim=2) - 1.) < 1e-4).all(), Qtb.X.sum(dim=2) - 1
+        assert (abs(Qtb.E.sum(dim=2) - 1.) < 1e-4).all()
+
+        # Compute transition probabilities
+        probX = X @ Qtb.X  # (bs, n, dx_out)
+        probE = E @ Qtb.E.unsqueeze(1)  # (bs, n, n, de_out)
+
+        sampled_t = diffusion_utils.sample_discrete_features(probX=probX, probE=probE, node_mask=node_mask)
+
+        X_t = F.one_hot(sampled_t.X, num_classes=self.Xdim_output)
+        E_t = F.one_hot(sampled_t.E, num_classes=self.Edim_output)
+        # print("X_t, E_t sizes: ", X_t.size(), E_t.size())
+        assert (X.shape == X_t.shape) and (E.shape == E_t.shape)
+
+        z_t = utils.PlaceHolder(X=X_t, E=E_t, y=y).type_as(X_t).mask(node_mask)
+
+        noisy_data = {'t_int': t_int, 't': t_float, 'beta_t': beta_t, 'alpha_s_bar': alpha_s_bar,
+                      'alpha_t_bar': alpha_t_bar, 'X_t': z_t.X, 'E_t': z_t.E, 'y_t': z_t.y, 'node_mask': node_mask}
+        return noisy_data
+
+    def compute_val_loss(self, pred, noisy_data, X, E, y, node_mask, test=False):
+        """Computes an estimator for the variational lower bound.
+           pred: (batch_size, n, total_features)
+           noisy_data: dict
+           X, E, y : (bs, n, dx),  (bs, n, n, de), (bs, dy)
+           node_mask : (bs, n)
+           Output: nll (size 1)
+       """
+        t = noisy_data['t']
+
+        # 1.
+        N = node_mask.sum(1).long()
+        log_pN = self.node_dist.log_prob(N)
+
+        # 2. The KL between q(z_T | x) and p(z_T) = Uniform(1/num_classes). Should be close to zero.
+        kl_prior = self.kl_prior(X, E, node_mask)
+
+        # 3. Diffusion loss
+        loss_all_t = self.compute_Lt(X, E, y, pred, noisy_data, node_mask, test)
+
+        # 4. Reconstruction loss
+        # Compute L0 term : -log p (X, E, y | z_0) = reconstruction loss
+        prob0 = self.reconstruction_logp(t, X, E, node_mask)
+
+        loss_term_0 = self.val_X_logp(X * prob0.X.log()) + self.val_E_logp(E * prob0.E.log())
+
+        # Combine terms
+        nlls = - log_pN + kl_prior + loss_all_t - loss_term_0
+        assert len(nlls.shape) == 1, f'{nlls.shape} has more than only batch dim.'
+
+        # Update NLL metric object and return batch nll
+        nll = (self.test_nll if test else self.val_nll)(nlls)        # Average over the batch
+
+        if wandb.run:
+            wandb.log({"kl prior": kl_prior.mean(),
+                       "Estimator loss terms": loss_all_t.mean(),
+                       "log_pn": log_pN.mean(),
+                       "loss_term_0": loss_term_0,
+                       'batch_test_nll' if test else 'val_nll': nll}, commit=False)
+        return nll
+
+    def forward(self, noisy_data, extra_data, node_mask):
+        X = torch.cat((noisy_data['X_t'], extra_data.X), dim=2).float()
+        E = torch.cat((noisy_data['E_t'], extra_data.E), dim=3).float()
+        y = torch.hstack((noisy_data['y_t'], extra_data.y)).float()
+        return self.model(X, E, y, node_mask)
+
 
 
 warnings.filterwarnings("ignore", category=PossibleUserWarning)
@@ -32,7 +196,7 @@ def get_resume(cfg, model_kwargs):
     name = cfg.general.name + '_resume'
     resume = cfg.general.test_only
     if cfg.model.type == 'discrete':
-        model = DiscreteDenoisingDiffusion.load_from_checkpoint(resume, **model_kwargs)
+        model = StepwiseDiffusion.load_from_checkpoint(resume, **model_kwargs)
     else:
         model = LiftedDenoisingDiffusion.load_from_checkpoint(resume, **model_kwargs)
     cfg = model.cfg
@@ -52,7 +216,7 @@ def get_resume_adaptive(cfg, model_kwargs):
     resume_path = os.path.join(root_dir, cfg.general.resume)
 
     if cfg.model.type == 'discrete':
-        model = DiscreteDenoisingDiffusion.load_from_checkpoint(resume_path, **model_kwargs)
+        model = StepwiseDiffusion.load_from_checkpoint(resume_path, **model_kwargs)
     else:
         model = LiftedDenoisingDiffusion.load_from_checkpoint(resume_path, **model_kwargs)
     new_cfg = model.cfg
@@ -166,7 +330,7 @@ def main(cfg: DictConfig):
     utils.create_folders(cfg)
 
     if cfg.model.type == 'discrete':
-        model = DiscreteDenoisingDiffusion(cfg=cfg, **model_kwargs)
+        model = StepwiseDiffusion(cfg=cfg, **model_kwargs)
     else:
         model = LiftedDenoisingDiffusion(cfg=cfg, **model_kwargs)
 
